38404ff24606d289e26f9c18dc6ff75338344cfa
yunjichoi9151 <cyjcode99@gmail.com>
2022-08-10 00:07:18 +0900

    Modify files

--- D1/Java_files/Code/Find_Middle_Number_2063.java
+++ D1/Java_files/Code/Find_Middle_Number_2063.java
@@ -1,16 +1,16 @@
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Find_Middle_Number_2063 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numArr[] = new int[200];
         int n = sc.nextInt();
+        int numArr[] = new int[n];
         for (int i = 0; i < n; i++) {
             numArr[i] = sc.nextInt();
         }
         Arrays.sort(numArr);
-        System.out.println(numArr[n / 2 + 1]);
+        System.out.println(numArr[n / 2]);
         sc.close();
     }
 }
--- D2/Java_files/Code/Rotating_Numeric_Array_1961.java
+++ D2/Java_files/Code/Rotating_Numeric_Array_1961.java
@@ -1,39 +1,30 @@
 import java.util.Scanner;
 
 public class Rotating_Numeric_Array_1961 {
-    static int[][] rotate(int arr[][]) {
-        int[][] new_arr = new int[arr.length][arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                new_arr[i][j] = arr[arr.length - 1 - j][i];
-            }
-        }
-        return new_arr;
-    }
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        for (int i = 1; i <= t; i++) {
+        for(int tc = 1; tc <= t; tc++) {
             int n = sc.nextInt();
-            int arr[][] = new int[n][n];
-            for (int j = 0; j < n; j++) {
-                for (int r = 0; r < n; r++) {
-                    arr[j][r] = sc.nextInt();
+            int[][] arr = new int[n][n];
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    arr[i][j] = sc.nextInt();
                 }
             }
-            int arr2[][] = rotate(arr);
-            int arr3[][] = rotate(arr2);
-            int arr4[][] = rotate(arr3);
-            System.out.format("#%d\n", i);
-            for (int j = 0; j < n; j++) {
-                for (int r = 0; r < n; r++)
-                    System.out.print(arr2[j][r]);
+            System.out.println("#" + tc);
+            for(int i = 0; i < n; i++) {
+                for(int k = 0; k < n; k++) {
+                    System.out.print(arr[n - 1 - k][i]);
+                }
                 System.out.print(" ");
-                for (int r = 0; r < n; r++)
-                    System.out.print(arr3[j][r]);
+                for(int k = 0; k < n; k++) {
+                    System.out.print(arr[n - 1 - i][n - 1 - k]);
+                }
                 System.out.print(" ");
-                for (int r = 0; r < n; r++)
-                    System.out.print(arr4[j][r]);
+                for(int k = 0; k < n; k++) {
+                    System.out.print(arr[k][n - 1 - i]);
+                }
                 System.out.println();
             }
         }
