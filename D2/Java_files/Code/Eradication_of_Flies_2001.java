8bae17bb3917b4e2691bf89bb9badc4ee78608e8
yunjichoi9151 <cyjcode99@gmail.com>
2022-08-12 00:04:13 +0900

    Modify file

--- D2/Java_files/Code/Eradication_of_Flies_2001.java
+++ D2/Java_files/Code/Eradication_of_Flies_2001.java
@@ -1,29 +1,38 @@
 import java.util.Scanner;
 
 public class Eradication_of_Flies_2001 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        for (int i = 0; i < t; i++) {
-            int n = sc.nextInt(), m = sc.nextInt(), max = 0;
-            int[][] arr = new int[n][n];
-            for (int j = 0; j < n; j++) {
-                for (int r = 0; r < n; r++)
-                    arr[j][r] = sc.nextInt();
-            }
-            for (int j = 0; j < n - m + 1; j++) {
-                for (int r = 0; r < n - m + 1; r++) {
-                    int count = 0;
-                    for (int x = 0; x < m; x++) {
-                        for (int y = 0; y < m; y++)
-                            count += arr[j + x][r + y];
-                    }
-                    if (max < count)
-                        max = count;
-                }
-            }
-            System.out.println("#" + (i + 1) + " " + max);
-        }
-        sc.close();
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int tc = 0; tc < t; tc++) {
+			int res = 0;
+			int n = sc.nextInt(), m = sc.nextInt();
+			int[][] arr = new int[n][n];
+			for(int i = 0; i < n; i++)
+				for(int j = 0; j < n; j++)
+					arr[i][j] = sc.nextInt();
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < n; j++) {
+					int sum1 = 0, sum2 = 0;
+					for(int x = -(m - 1); x < m; x++) {
+						if(i + x >= 0 && j >= 0 && i + x < n && j < n)
+							sum1 += arr[i + x][j];
+						if(i >= 0 && j + x >= 0 && i < n && j + x < n)
+							sum1 += arr[i][j + x];
+					}
+					sum1 -= arr[i][j];
+					for(int x = -(m - 1); x < m; x++) {
+						if(i + x >= 0 && j + x >= 0 && i + x < n && j + x < n)
+							sum2 += arr[i + x][j + x];
+						if(i + x >= 0 && j - x >= 0 && i + x < n && j - x < n)
+							sum2 += arr[i + x][j - x];
+					}
+					sum2 -= arr[i][j];
+					res = (sum1 > sum2 ? sum1 : sum2) > res ? (sum1 > sum2 ? sum1 : sum2) : res;
+				}
+			}
+			System.out.println("#" + (tc + 1) + " " + res);
+		}
+		sc.close();
+	}
 }
