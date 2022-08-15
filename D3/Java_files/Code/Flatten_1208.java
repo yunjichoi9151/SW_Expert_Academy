1ffa067633996e029af57c19f559e72c30143c69
yunjichoi9151 <cyjcode99@gmail.com>
2022-08-15 19:23:37 +0900

    Add 1208 file

---
+++ D3/Java_files/Code/Flatten_1208.java
@@ -0,0 +1,25 @@
+package Java_files.Code;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Flatten_1208 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int tc = 1; tc <= 10; tc++) {
+            int n = sc.nextInt();
+            int[] arr = new int[100];
+            for(int i = 0; i < 100; i++) {
+                arr[i] = sc.nextInt();
+            }
+            Arrays.sort(arr);
+            for(int i = 0; i < n; i++) {
+                arr[99]--;
+                arr[0]++;
+                Arrays.sort(arr);
+            }
+            System.out.println("#" + tc + " " + (arr[99] - arr[0]));
+        }
+        sc.close();
+    }    
+}
