27fff45adc74f8742a87aa28197e209f8d90a23a
yunjichoi9151 <cyjcode99@gmail.com>
2022-08-16 17:53:24 +0900

    Add 1220 file

---
+++ D3/Java_files/Code/Magnetic_1220.java
@@ -0,0 +1,35 @@
+package Java_files.Code;
+
+import java.util.Scanner;
+
+public class Magnetic_1220 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int tc = 1; tc <= 10; tc++) {
+            int n = sc.nextInt();
+            int[][] table = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    table[i][j] = sc.nextInt();
+                }
+            }
+ 
+            int cnt = 0;
+            int prev = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (table[j][i] == 1) {
+                        prev = 1;
+                    }
+                    if (prev == 1 && table[j][i] == 2) {
+                        cnt++;
+                        prev = 0;
+                    }
+                }
+                prev = 0;
+            }
+            System.out.println("#" + tc + " " + cnt);
+        }
+        sc.close();
+    }    
+}
