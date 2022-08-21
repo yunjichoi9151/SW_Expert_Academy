3a32b3582d258861b221ad4c45d291fde68a741e
yunjichoi9151 <cyjcode99@gmail.com>
2022-08-21 23:52:59 +0900

    Add 1228 file

---
+++ D3/Java_files/Code/Ciphertext1_1228.java
@@ -0,0 +1,33 @@
+package Java_files.Code;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ciphertext1_1228 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int tc = 1; tc <= 10; tc++) {
+            int n1 = sc.nextInt();
+            ArrayList<Integer> arr1 = new ArrayList<>();
+            for(int i = 0; i < n1; i++) {
+                arr1.add(sc.nextInt());
+            }
+            int n2 = sc.nextInt();
+            for(int i = 0; i < n2; i++) {
+                String s = sc.next();
+                int index = sc.nextInt();
+                int n3 = sc.nextInt();
+                for(int j = 0; j < n3; j++) {
+                    arr1.add(index, sc.nextInt());
+                    index++;
+                }
+            }
+            System.out.print("#" + tc + " ");
+            for(int i = 0; i < 10; i++) {
+                System.out.print(arr1.get(i) + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
